9aa02b0cbaaca3b87ac853f89b5ca7c5f1aacddb
Slava Pleshkov <mail@slavapleshkov.com>
2019-12-11 15:22:17 +0200

    Upgrade readme

--- t05/mx_pow.c
+++ t05/mx_pow.c
@@ -1,9 +1,14 @@
 double mx_pow(double n, unsigned int pow)
 {
-	double res = 1;
-	for (unsigned int i = 0; i < pow; ++i)
-	{
-		res *= n;
-	}
-	return res;
+    double result = 1;
+
+    if (pow == 0)
+        return result;
+    else if (pow > 0)
+    {
+        for (unsigned int i = 0; i < pow; i++)
+            result *= n;
+    }
+
+    return result;
 }
